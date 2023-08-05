127171ebaa52bdccdfd4f5a9af7a87b06ae49180
gayoungs <fairypetal0114@gmail.com>
2023-08-05 19:36:11 +0900

    2023.08.04

---
+++ src/main/java/com/simple/basic/command/UploadVO.java
@@ -0,0 +1,16 @@
+package com.simple.basic.command;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadVO {
+	
+	private MultipartFile file;
+
+}
